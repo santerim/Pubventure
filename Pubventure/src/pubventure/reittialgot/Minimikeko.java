31e01514b27bac45f5fa7d8cca3feb8041c04a6f
santerim <santerim@cs.helsinki.fi>
2013-05-27 20:10:03 +0300

    rukkaus

--- Pubventure/src/pubventure/reittialgot/Minimikeko.java
+++ Pubventure/src/pubventure/reittialgot/Minimikeko.java
@@ -1,13 +1,11 @@
-
 package pubventure.reittialgot;
 
 import pubventure.ymparisto.Pubiobjekti;
 
 /**
- *
  * @author Santeri
- * 
- * Minimikeko-toteutus Astar- ja Dijkstra-luokkien käyttöön
+ *
+ * Minimikeko-toteutus Astar- ja Dijkstra-luokkien käyttöön.
  * @see Astar
  * @see Dijkstra
  */
@@ -16,74 +14,114 @@ public class Minimikeko {
     private Pubiobjekti[] keko;
     private int pituus;
     private int solmuja;
-    
+
     public Minimikeko(int koko) {
         this.keko = new Pubiobjekti[koko];
     }
-    
+
     /**
      * heap-insert -toteutus
+     *
      * @param uusi on kekoon lisättävä Pubiobjekti
      */
     public void lisaaKekoon(Pubiobjekti uusi) {
         solmuja++;
-        int i = solmuja;
-        while (i > 1 && keko[vanhempi(i)].getF() > uusi.getF()) {
+        int i = solmuja - 1;
+        while (i > 0 && keko[vanhempi(i)].getF() > uusi.getF()) {
             keko[i] = keko[vanhempi(i)];
             i = vanhempi(i);
         }
         keko[i] = uusi;
     }
-    
+
     /**
      * heap-min -toteutus
+     *
      * @return palauttaa pienimmän f-arvon omaavan Pubiobjektin
      */
     public Pubiobjekti annaPienin() {
-        return keko[0];
+        if (solmuja > 0) {
+            return keko[0];
+        } else {
+            return null;
+        }
+
     }
-    
+
     /**
      * heap-del-min -toteutus
+     *
      * @return palauttaa pienimmän f-arvon omaavan Pubiobjektin, poistaa sen
      * keosta, ja järjestää keon uudelleen
      */
     public Pubiobjekti annaJaPoistaPienin() {
-        Pubiobjekti palautettava = keko[0];
-        keko[0] = keko[solmuja - 1];
-        solmuja--;
-        jarjesta(0);
-        return palautettava;
+        if (solmuja > 0) {
+            Pubiobjekti palautettava = keko[0];
+            keko[0] = keko[solmuja - 1];
+            solmuja--;
+            jarjesta(0);
+            return palautettava;
+        } else {
+            return null;
+        }
+
     }
-    
+
     /**
      * heapify-toteutus
+     *
      * @param mista on indeksi mistä alkaen kekoa aletaan järjestää
      */
     private void jarjesta(int mista) {
+        int vasen = vasenLapsi(mista);
+        int oikea = oikeaLapsi(mista);
+        int pienempi;
+        // tämän tarkoitus on olla vain apuna vaihdettaessa Pubiobjektien
+        // paikkaa keossa
+        Pubiobjekti narri;
         
+        if (oikea < solmuja) {
+            if (keko[vasen].getF() > keko[oikea].getF()) {
+                pienempi = oikea;
+            } else {
+                pienempi = vasen;
+            }
+            if (keko[mista].getF() > keko[pienempi].getF()) {
+                narri = keko[mista];
+                keko[mista] = keko[pienempi];
+                keko[pienempi] = narri;
+                jarjesta(pienempi);
+            }
+        } else if (vasen == solmuja && keko[mista].getF() > keko[vasen].getF()) {
+            narri = keko[mista];
+            keko[mista] = keko[vasen];
+            keko[vasen] = narri;
+        }
     }
-    
+
     /**
      * metodi palauttaa solmun vanhemman indeksinumeron keossa
+     *
      * @param sija on indeksinumero solmulle, jonka vanhempi halutaan saada
      * selville
      */
     private int vanhempi(int sija) {
         return ((sija + 1) / 2) - 1;
     }
-    
+
     /**
      * metodi palauttaa solmun vasemman lapsen
+     *
      * @param sija on lastaan kaipaavan solmun indeksinumero keossa
      * @return palauttaa vasemman lapsen indeksinumeron
      */
     private int vasenLapsi(int sija) {
         return ((sija + 1) * 2) - 1;
     }
-    
+
     /**
      * metodi palauttaa solmun oikean lapsen
+     *
      * @param sija on lastaan kaipaavan solmun indeksinumero keossa
      * @return palauttaa oikean lapsen indeksinumeron
      */
---
+++ Pubventure/test/pubventure/reittialgot/MinimikekoTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pubventure.reittialgot;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Santeri
+ */
+public class MinimikekoTest {
+    
+    public MinimikekoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of lisaaKekoon method, of class Minimikeko.
+     */
+    @Test
+    public void testLisaaKekoon() {
+    }
+
+    /**
+     * Test of annaPienin method, of class Minimikeko.
+     */
+    @Test
+    public void testAnnaPienin() {
+    }
+
+    /**
+     * Test of annaJaPoistaPienin method, of class Minimikeko.
+     */
+    @Test
+    public void testAnnaJaPoistaPienin() {
+    }
+}
